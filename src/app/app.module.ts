3e6c275f6adccf461c10cc3078bcf4dc3ac83dd3
platov2pac <ilya.platow.15@gmail.com>
2021-09-22 17:35:25 +0300

    login started flex!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {ButtonModule} from 'primeng/button'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from "@angular/router";
+import {LoginComponent} from './components/login/login.component';
+import {RippleModule} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+    ]),
+    RippleModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
